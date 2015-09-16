d6ed8cf9d27c11af061facd07e0ff82a04fdf1a2
asakusayumeka <asakusayumeka@gmail.com>
2015-09-16 11:34:56 +0700

    Set opencard

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:100/0915_ShopThoiTrang/admin/');
+define('HTTP_CATALOG', 'http://localhost:100/0915_ShopThoiTrang/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:100/0915_ShopThoiTrang/admin/');
+define('HTTPS_CATALOG', 'http://localhost:100/0915_ShopThoiTrang/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/www/0915_ShopThoiTrang/admin/');
+define('DIR_SYSTEM', 'D:/www/0915_ShopThoiTrang/system/');
+define('DIR_LANGUAGE', 'D:/www/0915_ShopThoiTrang/admin/language/');
+define('DIR_TEMPLATE', 'D:/www/0915_ShopThoiTrang/admin/view/template/');
+define('DIR_CONFIG', 'D:/www/0915_ShopThoiTrang/system/config/');
+define('DIR_IMAGE', 'D:/www/0915_ShopThoiTrang/image/');
+define('DIR_CACHE', 'D:/www/0915_ShopThoiTrang/system/cache/');
+define('DIR_DOWNLOAD', 'D:/www/0915_ShopThoiTrang/system/download/');
+define('DIR_UPLOAD', 'D:/www/0915_ShopThoiTrang/system/upload/');
+define('DIR_LOGS', 'D:/www/0915_ShopThoiTrang/system/logs/');
+define('DIR_MODIFICATION', 'D:/www/0915_ShopThoiTrang/system/modification/');
+define('DIR_CATALOG', 'D:/www/0915_ShopThoiTrang/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '0915_ShopThoiTrang');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:100/0915_ShopThoiTrang/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:100/0915_ShopThoiTrang/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/www/0915_ShopThoiTrang/catalog/');
+define('DIR_SYSTEM', 'D:/www/0915_ShopThoiTrang/system/');
+define('DIR_LANGUAGE', 'D:/www/0915_ShopThoiTrang/catalog/language/');
+define('DIR_TEMPLATE', 'D:/www/0915_ShopThoiTrang/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/www/0915_ShopThoiTrang/system/config/');
+define('DIR_IMAGE', 'D:/www/0915_ShopThoiTrang/image/');
+define('DIR_CACHE', 'D:/www/0915_ShopThoiTrang/system/cache/');
+define('DIR_DOWNLOAD', 'D:/www/0915_ShopThoiTrang/system/download/');
+define('DIR_UPLOAD', 'D:/www/0915_ShopThoiTrang/system/upload/');
+define('DIR_MODIFICATION', 'D:/www/0915_ShopThoiTrang/system/modification/');
+define('DIR_LOGS', 'D:/www/0915_ShopThoiTrang/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '0915_ShopThoiTrang');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
